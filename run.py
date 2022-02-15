6d6e7bea0a888393531eb59d6b36b9a8f68f2d7f
Jonnywdev <jonnywhitt19@gmail.com>
2022-02-15 18:23:06 +0000

    Add really basic initial if statement

--- run.py
+++ run.py
@@ -4,7 +4,28 @@ This first section will hold the first print and input statements, welcoming the
 
 print("Welcome to Adventure Stories! \n")
 characterName = input("What would you like your characters name to be? \n")
-print("Which story would you like to play? \n Pirate, Dark Forrest or City") 
+print("Which story would you like to play? \n Forrest or City") 
 
-selectedStory = input("Please type the name of the story you want to play.")
+selectedStory = input("Please type the name of the story you want to play. ").lower()
 
+"""
+This if statement will check to see what story has been selected.
+"""
+
+if selectedStory == "forrest":
+    print(2)
+else:
+    print(3)
+
+
+
+
+
+
+# forrestAdventureGame = print(2)
+
+# cityAdventureGame = print(3)
+
+
+# forrestAdventureGame()
+# cityAdventureGame()
