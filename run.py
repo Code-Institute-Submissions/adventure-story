e190eeec5db7958f280558f6fe34e079556b80a6
Jonnywdev <jonnywhitt19@gmail.com>
2022-02-15 18:03:20 +0000

    Add initial structure to first section of application

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 """
-This will be the only file and hold the entire game.
+This first section will hold the first print and input statements, welcoming the player to the game
 """
+
+print("Welcome to Adventure Stories! \n")
+characterName = input("What would you like your characters name to be? \n")
+print("Which story would you like to play? \n Pirate, Dark Forrest or City") 
+
+selectedStory = input("Please type the name of the story you want to play.")
 
