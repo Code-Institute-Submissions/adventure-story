9cd011ba3eab4055d201b72c0962ee6342031cb8
Jonnywdev <jonnywhitt19@gmail.com>
2022-02-16 02:19:58 +0000

    Add first set for the story

--- run.py
+++ run.py
@@ -27,36 +27,56 @@ def forestAdventureGame():
         """
         Second question, do they go left or right?
         """
-        left_or_right = input("\n You have just got to the forest, there are 2 paths. \n But it is to dark to read the signs, do you take the left or right path? (left/right)").lower()
+        left_or_right = input("\n There are 2 paths ahead. \n But it is to dark to read the signs, do you take the left or right path? (left/right)").lower()
         if left_or_right == "left":
             
             """
             Third Question, do they go to the house or not? go_or_no_go
             """
-            go_or_no_go = input("\n You have gone down the left path and see a small hut with lights on up ahead, do you go to it or go back to where you came from? (go/turn back)") 
+            go_or_no_go = input("\n You have gone down the left path and see a small hut with lights on up ahead, do you go to it or go back to where you came from? (go/turn back)").lower() 
             
             """
-            Player has decied to go towards the hut.
+            Player has decied to go towards the hut. This ends the Story!
             """
             if go_or_no_go == "go":
-                print("\n A man stepped out of the hut with a knife, you ran away.")
+                print("\n A man stepped out of the hut with a knife, you ran away. \n GAME OVER")
 
-            """
-            Player has decied to turn back.
-            """
-            elif go_or_no_go == "turn back"
+                """
+                Player has decied to turn back.
+                """
+            elif go_or_no_go == "turn back":
 
+                """
+                Player gets back to the 2 paths, do they take the go home or go right?
+                """
+                go_home_or_go_right = input("\n You are now back to where you started,\n do you want to go back home or the right? (home/right)")
+                if go_home_or_go_right == "home":
+                    print("Have some rest and try again. \n GAME OVER")
 
-            """
-            Player has entered an incorrect answer, end's the game.
-            """
+                elif go_home_or_go_right == "right":
+
+                else:
+                print("Uh oh, that wasn't an available answer. \n GAME OVER!")
+                """
+                Player has entered an incorrect answer, end's the game.
+                """
             else:
                 print("Uh oh, that wasn't an available answer. \n GAME OVER!")
 
         elif left_or_right == "right":
             """
-            Second question (after player decided to go right), ?
+            Second question (after player decided to go right), they hear a weird noise, do they explore or go home?
             """
+            explore_or_go_home = input("You hear a weird noise, do you carry on and investigate the noise or go home? \n (investigate/go home)")
+            
+            if explore_or_go_home == "investigate":
+                print("You get surrounded by wolfs! \n GAME OVER")
+
+            elif explore_or_go_home == "go home":
+                print("Have some rest and try again. \n GAME OVER")
+
+            else:
+                print("Uh oh, that wasn't an available answer. \n GAME OVER!"
             
         else:
             print("Uh oh, that wasn't an available answer. \n GAME OVER!")
@@ -66,8 +86,9 @@ def forestAdventureGame():
         """
     elif day_or_night == "wait":
         """
-        Second question, do they go left or right?
+        Second question, do they go ?
         """
+
 
         """
         Player has entered an incorrect answer, end's the game.
