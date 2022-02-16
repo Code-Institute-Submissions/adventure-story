db72d51570e4f9090953fd1b1cbb2c1355252487
Jonnywdev <jonnywhitt19@gmail.com>
2022-02-16 00:22:40 +0000

    Add new questions to game

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ This first section will hold the first print and input statements, welcoming the
 """
 
 print("Welcome to Adventure Stories! \n")
-characterName = input("What would you like your characters name to be? \n")
 
 print("Rules of the game: \n")
 print("There is only one rule, you can only type an that is given to you. \n If you type anything else your story will be over!")
@@ -20,7 +19,7 @@ def forestAdventureGame():
     """
     This will be the first question in the story.
     """
-    day_or_night = input(characterName "is ready to enter the forest, but its almost night, \n do they still go or do they wait until morning? (go now/wait) ").lower()
+    day_or_night = input( "you are ready to enter the forest, but its almost night, \n do you still go or do they wait until morning? (go now/wait) \n").lower()
     """
     Player has selected to go at night
     """
@@ -28,25 +27,51 @@ def forestAdventureGame():
         """
         Second question, do they go left or right?
         """
-        left_or_right = input(characterName "has just got to the forest, there are 2 paths. \n But it is to dark to read the signs, do they take the left or right path? (left/right)").lower()
+        left_or_right = input("\n You have just got to the forest, there are 2 paths. \n But it is to dark to read the signs, do you take the left or right path? (left/right)").lower()
         if left_or_right == "left":
-            print("woo")
-        elif left_or_right == "right":
-            print("ooo")
-        else:
-        print("Uh oh, that wasn't an available answer. \n GAME OVER!")
+            
+            """
+            Third Question, do they go to the house or not? go_or_no_go
+            """
+            go_or_no_go = input("\n You have gone down the left path and see a small hut with lights on up ahead, do you go to it or go back to where you came from? (go/turn back)") 
+            
+            """
+            Player has decied to go towards the hut.
+            """
+            if go_or_no_go == "go":
+                print("\n A man stepped out of the hut with a knife, you ran away.")
 
-    """
-    Player has selected to wait until morning.
-    """
+            """
+            Player has decied to turn back.
+            """
+            elif go_or_no_go == "turn back"
+
+
+            """
+            Player has entered an incorrect answer, end's the game.
+            """
+            else:
+                print("Uh oh, that wasn't an available answer. \n GAME OVER!")
+
+        elif left_or_right == "right":
+            """
+            Second question (after player decided to go right), ?
+            """
+            
+        else:
+            print("Uh oh, that wasn't an available answer. \n GAME OVER!")
+
+        """
+        Player has selected to wait until morning.
+        """
     elif day_or_night == "wait":
         """
         Second question, do they go left or right?
         """
 
-    """
-    Player has entered an incorrect answer, end's the game.
-    """
+        """
+        Player has entered an incorrect answer, end's the game.
+        """
     else:
         print("Uh oh, that wasn't an available answer. \n GAME OVER!")
 
@@ -62,7 +87,7 @@ This if statement will check to see what story has been selected.
 """
 
 if selectedStory == "forest":
-    forrestAdventureGame()
+    forestAdventureGame()
 elif selectedStory == "city":
     cityAdventureGame()
 else:
@@ -78,5 +103,5 @@ else:
 
 
 
-# forrestAdventureGame()
-# cityAdventureGame()
+forestAdventureGame()
+cityAdventureGame()
